1b53e5f7f7b762a99c96d4ad181761685161a311
Mike Huff <mikehuff@amazon.com>
2016-12-22 12:07:49 -0800

    Initial Commit

---
+++ test_soundboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test_soundboard
+//
+//  Created by mikehuff on 12/22/16.
+//  Copyright © 2016 mikehuff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
