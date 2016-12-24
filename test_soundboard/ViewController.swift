01490c9f68e38447e83aacb0e61b1bbe4048887e
Mike Huff <mikehuff@amazon.com>
2016-12-24 01:50:48 -0800

    works to display charles name

--- test_soundboard/ViewController.swift
+++ test_soundboard/ViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var myTableView: UITableView!
+    
+    
+    class SoundByte {
+        let name: String
+        let internalName: String
+        init(mName:String, mInternalName : String) {
+            name = mName
+            internalName = mInternalName
+        }
+    }
+    
+    
+    let soundBytes = [
+        SoundByte(mName: "thank you", mInternalName: "IB_jizomida"),
+        SoundByte(mName: "hello", mInternalName: "IB_awnyawng"),]
+    
+    // needed for cell recycling
+    let cellIdentifier = "asdfasdf"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //maybe don't need these anymore
+//        myTableView.dataSource = self
+//        myTableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+     return 10
+    }
+    
+    
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = "Thanks Charles!"
+            
+//            soundBites[indexPath.row]
+        return cell
+    }
 
-
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+     print("clicked cell \(indexPath.row)")
+    }
+    
 }
 
