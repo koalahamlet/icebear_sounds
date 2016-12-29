9d4c3ef27b5750d940ba07a8fd8f55125ef40610
Mike Huff <mikehuff@amazon.com>
2016-12-29 14:20:52 -0800

    successfully add more sounds

--- test_soundboard/ViewController.swift
+++ test_soundboard/ViewController.swift
@@ -24,9 +24,117 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         }
     }
     
+//    
+//    {
+//    name: "tote wrap",
+//    url: "tote_wrap.mp3"
+//    },
+//    {
+//    name: "tote life",
+//    url: "IB_tote_life.mp3"
+//    },
+//    {
+//    name: "best thing",
+//    url: "IB_best_thing_to_happen.mp3"
+//    },
+//    {
+//    name: "enjoys basking",
+//    url: "IB_enjoys_basking.mp3"
+//    },
+//    {
+//    name: "believed in you",
+//    url: "IB_believed_in_you.mp3"
+//    },
+//    {
+//    name: "will coach you",
+//    url: "IB_coach_you_manhood.mp3"
+//    },
+//    {
+//    name: "still proud",
+//    url: "IB_is_still_proud.mp3"
+//    },
+//    {
+//    name: "thinks you're precious",
+//    url: "IB_thinks_youre_precious.mp3"
+//    },
+//    {
+//    name: "will not bless you",
+//    url: "IB_will_not_bless_you.mp3"
+//    },
+//    {
+//    name: "top bear now",
+//    url: "IB_wants_top_bear.mp3"
+//    },
+//    {
+//    name: "needs bug spray",
+//    url: "IB_bug_spray.mp3"
+//    },
+//    {
+//    name: "is survivor",
+//    url: "IB_is_survivor.mp3"
+//    },
+//    {
+//    name: "made her a pashmina",
+//    url: "IB_made_a_pajmina.mp3"
+//    },
+//    {
+//    name: "settled that",
+//    url: "IB_settled_that.mp3"
+//    },
+//    {
+//    name: "likes turtle",
+//    url: "IB_turtle.mp3"
+//    },
+//    {
+//    name: "don't ditch",
+//    url: "IB_dont_ditch.mp3"
+//    },
+//    {
+//    name: "turtleneck",
+//    url: "IB_turtle_neck.mp3"
+//    },
+//    {
+//    name: "miss yoga",
+//    url: "IB_miss_yoga.mp3"
+//    },
+//    {
+//    name: "smells like clean babies",
+//    url: "IB_cleanbabies.mp3"
+//    },
+//    {
+//    name: "hates butts",
+//    url: "IB_hates_butts.mp3"
+//    },
+    
     
     let soundBytes = [
-        SoundByte(mName: "thank you", mInternalName: "IB_jizomida"),
+        SoundByte(mName: "Merry Xmas Min!", mInternalName: "IB_koreanxmasheaven"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+//        SoundByte(mName: "", mInternalName: "IB_"),
+        SoundByte(mName: "is in", mInternalName: "IB_is_in"),
+        SoundByte(mName: "too many secrets", mInternalName: "IB_too_many_secrets"),
+        SoundByte(mName: "ossuoshipshoe", mInternalName: "IB_ossuoshipshoe"),
+        SoundByte(mName: "poodi something", mInternalName: "IB_poogi_something"),
+        SoundByte(mName: "shilliyagisamnidad", mInternalName: "IB_shilliyagisamnidad"),
+        SoundByte(mName: "jizomida", mInternalName: "IB_jizomida"),
         SoundByte(mName: "hello", mInternalName: "IB_awnyawng"),]
     
     // needed for cell recycling
